c33b06ee7e81977a604734dfc5bf50b7587afa8d
mmdmak1111-blip <mmdmak.1111@gmail.com>
2026-05-29 19:13:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function showMessage() {
-  alert("به سایت حرفه‌ای من خوش اومدی 😎");
+const chatBox = document.getElementById("chat-box");
+
+function loadMessages() {
+  const messages = JSON.parse(localStorage.getItem("messages")) || [];
+
+  chatBox.innerHTML = "";
+
+  messages.forEach(msg => {
+    chatBox.innerHTML += `
+      <div class="message">
+        <strong>${msg.username}</strong>
+        <p>${msg.text}</p>
+      </div>
+    `;
+  });
+
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
+
+function sendMessage() {
+  const username = document.getElementById("username").value;
+  const message = document.getElementById("message").value;
+
+  if (!username || !message) return;
+
+  const messages = JSON.parse(localStorage.getItem("messages")) || [];
+
+  messages.push({
+    username: username,
+    text: message
+  });
+
+  localStorage.setItem("messages", JSON.stringify(messages));
+
+  document.getElementById("message").value = "";
+
+  loadMessages();
+}
+
+loadMessages();
